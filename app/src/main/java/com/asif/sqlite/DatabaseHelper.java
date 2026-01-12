a9f54e6523d18c70425486fd4889962efab1b86e
Asif Al Rumel <157966985+asifff009@users.noreply.github.com>
2026-01-12 19:02:21 +0600

    insert data to SQL

---
+++ app/src/main/java/com/asif/sqlite/DatabaseHelper.java
@@ -0,0 +1,44 @@
+package com.asif.sqlite;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    public static final String DB_NAME = "my_database";
+    public static final int DB_VERSION = 1;
+
+
+    public DatabaseHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // ekhane table create kora lagee....
+
+        db.execSQL("create table my_table (id INTEGER primary key autoincrement, name TEXT, mobile TEXT)");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("drop table if exists my_table ");
+    }
+
+    public void insertData(String name, String mobile){
+        SQLiteDatabase database = this.getWritableDatabase();
+
+        ContentValues conval = new ContentValues();
+        conval.put("name", name);
+        conval.put("mobile", mobile);
+
+        database.insert("my_table", null, conval);
+
+    }
+
+
+}
---
+++ app/src/main/java/com/asif/sqlite/MainActivity.java
@@ -0,0 +1,50 @@
+package com.asif.sqlite;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edName, edMobile;
+    Button buttonInsert, buttonShow;
+
+    DatabaseHelper dbHelper;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edName = findViewById(R.id.edName);
+        edMobile = findViewById(R.id.edMobile);
+        buttonInsert = findViewById(R.id.buttonInsert);
+        buttonShow = findViewById(R.id.buttonShow);
+
+        dbHelper = new DatabaseHelper(MainActivity.this);
+
+        buttonInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // insert er code hobe ekhane
+
+                dbHelper.insertData(edName.getText().toString(), edMobile.getText().toString());
+                Toast.makeText(MainActivity.this, "Data has been inserted", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+    }
+
+    //----------------------------------------------
+}
